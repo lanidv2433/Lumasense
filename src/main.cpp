d323e47014a41b6917aa1bab5031888fd99a7544
Roy Richa <rricha@uci.edu>
2024-12-04 19:03:52 -0800

    Motion sensor working - installed pulsesensor library

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,63 @@
-// #include <Arduino.h>
-
-
-
-// const int LED_PIN = 17;
-
-// void setup(){
-//   pinMode(LED_PIN, OUTPUT);
-// }
-
-// void loop(){
-//   digitalWrite(LED_PIN, 1);
-
-// }
-
 #include <Arduino.h>
 #include <FastLED.h>
 #include <VoiceRecognitionV3.h>
 #include <SoftwareSerial.h>
-#define LED_PIN     13
-#define NUM_LEDS    166
-#define BULB_PIN 17
+#include <PulseSensorPlayground.h>
+
+#define LED_PIN 13
+#define NUM_LEDS 166
+#define BULB_PIN 17 // LED to blink on pulse or motion
+#define PULSE_INPUT 32
+#define THRESHOLD 525
+#define MOTION_SENSOR_PIN 15 // IR motion sensor input pin
 
 SoftwareSerial voiceRecognition(22, 21); // RX on pin 22, TX on pin 21
-
 CRGB leds[NUM_LEDS];
-void setup() {
+
+// PulseSensor setup
+PulseSensorPlayground pulseSensor;
+
+// Motion sensor state
+int pirState = LOW; // Start with no motion detected
+
+void setup()
+{
+  // FastLED setup
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-  Serial.begin(9600);
+  FastLED.setBrightness(50);
+
+  // Serial communication
+  Serial.begin(9600); // Set baud rate to 9600
   voiceRecognition.begin(9600);
-  pinMode(BULB_PIN, OUTPUT);
+
+  // Bulb pin and PulseSensor setup
+  pinMode(BULB_PIN, OUTPUT);         // Set BULB_PIN as output
+  pinMode(MOTION_SENSOR_PIN, INPUT); // Set motion sensor as input
+  pulseSensor.analogInput(PULSE_INPUT);
+  pulseSensor.blinkOnPulse(-1); // Disable library's blink-on-pulse feature
+  pulseSensor.setSerial(Serial);
+  pulseSensor.setOutputType(SERIAL_PLOTTER);
+  pulseSensor.setThreshold(THRESHOLD);
+
+  // Disable hardware timing
+  pulseSensor.UsingHardwareTimer = false;
+
+  // Initialize PulseSensor
+  if (!pulseSensor.begin())
+  {
+    while (true)
+    {
+      // Indicate initialization failure by blinking BULB_PIN
+      digitalWrite(BULB_PIN, LOW);
+      delay(100);
+      digitalWrite(BULB_PIN, HIGH);
+      delay(100);
+    }
+  }
+
+  // Turn off the LED strip initially
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+  FastLED.show();
 }
 
 void handleCommand(String command)
@@ -39,9 +68,19 @@ void handleCommand(String command)
   }
   else if (command == "blink")
   {
-    digitalWrite(LED_PIN, HIGH); // LED_PIN is the pin number of the LED you want to blink
-    delay(1000);                 // delay for 1 second (1000 milliseconds)
-    digitalWrite(LED_PIN, LOW);  // turn the LED off again
+    digitalWrite(LED_PIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_PIN, LOW);
+  }
+  else if (command == "brightness low")
+  {
+    FastLED.setBrightness(50);
+    Serial.println("Brightness set to low.");
+  }
+  else if (command == "brightness high")
+  {
+    FastLED.setBrightness(200);
+    Serial.println("Brightness set to high.");
   }
   else
   {
@@ -49,29 +88,84 @@ void handleCommand(String command)
   }
 }
 
-void loop() {
-  digitalWrite(BULB_PIN, 1);
+void loop()
+{
+  // // Handle voice commands
+  // if (voiceRecognition.available())
+  // {
+  //   String command = voiceRecognition.readString();
+  //   Serial.println("Command received: " + command);
+  //   handleCommand(command);
+  // }
 
-  if (voiceRecognition.available())
+  // Heartbeat detection
+  if (pulseSensor.sawStartOfBeat())
   {
-    String command = voiceRecognition.readString();
-    Serial.println("Command received:" + command);
-    handleCommand(command);
+    Serial.println("Heartbeat detected!");
+    // pulseSensor.outputBeat();
+
+    // Blink LED on BULB_PIN for heartbeat
+    digitalWrite(BULB_PIN, HIGH);
+    delay(50);
+    digitalWrite(BULB_PIN, LOW);
   }
 
-  for (int i = 0; i <= 166; i++) {         //Red goes one round
-    leds[i] = CRGB ( 160, 32, 255);
+  // Motion detection
+  int motionDetected = digitalRead(MOTION_SENSOR_PIN); // Read motion sensor
+  if (motionDetected == HIGH)
+  {
+    fill_solid(leds, NUM_LEDS, CRGB::White); // Turn on LED strip (set to white)
     FastLED.show();
-    delay(5);
+
+    if (pirState == LOW)
+    {
+      Serial.println("Motion detected!");
+      pirState = HIGH;
+    }
   }
-  for (int i = 0; i <= 166; i++) {         //Green goes one round 
-    leds[i] = CRGB ( 80, 208, 255);
+  else
+  {
+    fill_solid(leds, NUM_LEDS, CRGB::Black); // Turn off LED strip
     FastLED.show();
-    delay(5);
+
+    if (pirState == HIGH)
+    {
+      Serial.println("Motion ended!");
+      pirState = LOW;
+    }
   }
-  for (int i = 0; i <= 166; i++) {         //Blue goes one round 
-    leds[i] = CRGB ( 255, 0, 0);
-    FastLED.show();
-    delay(5);
+
+  // Heartbeat sensor periodic update
+  if (pulseSensor.UsingHardwareTimer)
+  {
+
+    /*
+       Wait a bit.
+       We don't output every sample, because our baud rate
+       won't support that much I/O.
+    */
+    delay(20);
+    // write the latest sample to Serial.
+    pulseSensor.outputSample();
+  }
+  else
+  {
+    /*
+        When using a software timer, we have to check to see if it is time
+        to acquire another sample. A call to sawNewSample will do that.
+    */
+    if (pulseSensor.sawNewSample())
+    {
+      /*
+          Every so often, send the latest Sample.
+          We don't print every sample, because our baud rate
+          won't support that much I/O.
+      */
+      if (--pulseSensor.samplesUntilReport == (byte)0)
+      {
+        pulseSensor.samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
+        pulseSensor.outputSample();
+      }
+    }
   }
 }
